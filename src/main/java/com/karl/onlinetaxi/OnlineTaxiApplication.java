bb4d00fba16e19d08314066e6a5d1f57db229a1f
SasaKiHaise <2846698558@qq.com>
2022-07-05 16:23:50 +0800

    Initial commit

---
+++ src/main/java/com/karl/onlinetaxi/OnlineTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.karl.onlinetaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineTaxiApplication.class, args);
+    }
+
+}
